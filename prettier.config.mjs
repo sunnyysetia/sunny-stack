defedf7eb0edc65195ba52efbfedb6ad0f4a39b7
Sunny Setia <sunnyysetia@gmail.com>
2025-12-18 10:56:25 +1300

    added prettier globally

---
+++ prettier.config.mjs
@@ -0,0 +1,10 @@
+const config = {
+    printWidth: 100,
+    singleQuote: true,
+    semi: true,
+    trailingComma: "all",
+    plugins: ["prettier-plugin-tailwindcss"],
+    tailwindStylesheet: "./apps/dashboard/src/styles/tailwind.css"
+  }
+
+  export default config;
